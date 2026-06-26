8c5e7ca6336706137c3de30e3a0d17221cc779c1
apatil210 <apatil2@lbl.gov>
2026-06-26 14:33:22 -0700

    Update Draft2.py

--- Draft2.py
+++ Draft2.py
@@ -230,18 +230,18 @@ if submitted:
 
             r1, r2, r3, r4 = st.columns(4)
             # r1.metric("HX Area (m²)", f"{area:.4f}")
-            r2.metric("Calculated U (W/m²-K)", f"{u:.2f}")
+            r1.metric("Calculated U (W/m²-K)", f"{u:.2f}")
             # r3.metric("UA (W/K)", f"{result['UA']:.2f}")
             # r4.metric("NTU", f"{result['NTU']:.4f}")
 
-            r5, r6, r7 = st.columns(3)
+            
             # r5.metric("Effectiveness", f"{result['Effectiveness']:.4f}")
-            r6.metric("Heat Duty Q (kW)", f"{result['Q_kW']:.4f}")
+            r2.metric("Heat Duty Q (kW)", f"{result['Q_kW']:.4f}")
             # r7.metric("Capacity Ratio C_r", f"{result['C_r']:.4f}")
 
             r8, r9 = st.columns(2)
-            r8.metric("Hot Outlet Temp (°C)", f"{result['T_h_out']:.2f}")
-            r9.metric("Cold Outlet Temp (°C)", f"{result['T_c_out']:.2f}")
+            r3.metric("Hot Outlet Temp (°C)", f"{result['T_h_out']:.2f}")
+            r4.metric("Cold Outlet Temp (°C)", f"{result['T_c_out']:.2f}")
 
             st.subheader("Shell-and-tube cost result")
 
